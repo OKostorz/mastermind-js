8874e36d8a4c5bd47fdff3b1dd67dc0f13ebedb3
livv <livv>
2018-01-15 10:05:16 +0100

    no message

---
+++ js/mastermind.js
@@ -0,0 +1,14 @@
+var colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+var max_row = 12;
+var max_column = 4;
+var current_row = 1;
+var answer = [];
+
+
+for(var i=0; i<4; i++){
+    var colorNum = Math.round(Math.random()*5);
+    answer[i]=colorNum;
+}
+console.log(answer);
+
+
