ab443881d2e93f5521197ee2e44652d524747670
livv <livv>
2018-01-24 09:34:45 +0100

    no message

--- js/mastermind.js
+++ js/mastermind.js
@@ -3,7 +3,7 @@ var max_row = 12;
 var max_column = 4;
 var current_row = 1;
 var answer = [];
-
+var colour = "";
 
 for(var i=0; i<4; i++){
     var colorNum = Math.round(Math.random()*5);
@@ -11,4 +11,13 @@ for(var i=0; i<4; i++){
 }
 console.log(answer);
 
+function color(kleur){
+    colour = kleur;
+}
 
+function changeColor(change){
+    
+}
+   
+    
+    
